b42f67e26e8ce607541aeae425c75ed89c223f57
Dmitri <dmitri.antipov1@gmail.com>
2022-08-09 20:33:57 +0300

    Sergei Stavitski 09.08.2022

---
+++ script.js
@@ -0,0 +1,26 @@
+const countdown = () => {
+    // const countDate = new Date('April 14, 2022 13:55:00').getTime()
+    const countDate = new Date('August 09, 2022 13:53:00').getTime()
+    const now = new Date().getTime()
+    const gap = now - countDate
+
+    const second = 1000;
+    const minute = second * 60
+    const hour = minute * 60
+    const day = hour * 24
+
+    const textDay = Math.floor(gap / day)
+    const textHour = Math.floor((gap % day) / hour)
+    const textMinute = Math.floor((gap % hour) / minute)
+    const textSecond = Math.floor((gap % minute) / second)
+
+    document.querySelector('#day').innerText = textDay
+    document.querySelector('#hour').innerText = textHour
+    document.querySelector('#minute').innerText = textMinute
+    document.querySelector('#second').innerText = textSecond
+}
+
+setInterval(countdown, 1000)
+
+let card = document.querySelector('#result');
+// card.style.width = 100 + '%'
